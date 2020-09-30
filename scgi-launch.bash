e5456f7f896af89ffb38cd0e0c6a90ffacae7c7e
Steven Fitzgerald <steve@csun.edu>
2020-09-30 11:35:47 -0700

    Fixed path typeo

--- scgi-launch.bash
+++ scgi-launch.bash
@@ -31,7 +31,7 @@ ADDR=$1
 PORT=$2 
 CGI_PROGRAM="${PWD}/$3"
 
-SCGI2ENV_EXEC="${PWD}$(dirname $0)/scgi2env-exec"
+SCGI2ENV_EXEC="${PWD}/$(dirname $0)/scgi2env-exec"
 
 [ $# == 3 ]             || { echo "Usage: scgi-launch ADDR PORT CGI_PROGRAM" ; exit 1 ; }
 [ -f ${SCGI2ENV_EXEC} ] || { echo "Error: scgi2env-exec program not found"   ; exit 1 ; }
