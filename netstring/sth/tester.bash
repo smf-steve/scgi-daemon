745940c0db90cddfe8714c42005fd49c185e2b2e
Steve Fitzgerald <smf.steve.fitzgerald@gmail.com>
2026-09-19 13:00:48 -0700

    added test cases and improved readbility of output

--- netstring/sth/tester.bash
+++ netstring/sth/tester.bash
@@ -2,6 +2,6 @@
 
 FILE=${1}
 
-../netstring -e < ${FILE} > encoded_${FILE}
-../netstring -d < encoded_${FILE} > decoded_${FILE}
-diff --brief  ${FILE} decoded_${FILE}
+echo "encoding: " ; ../netstring -e < ${FILE} > encoded_${FILE}
+echo "decoding: " ; ../netstring -d < encoded_${FILE} > decoded_${FILE}
+echo "diff: "     ; diff --brief  ${FILE} decoded_${FILE}
